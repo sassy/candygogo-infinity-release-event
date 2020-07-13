2c73b47b1ab7e25b331b23ad3544f72955323c83
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-07-14 02:45:21 +0900

    add base contents.

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import Logo from '../images/header_logo.jpg'
+
+function Heaader() {
+    return (
+        <header className="App-header">
+            <h2><img className="Header-logo" src={Logo}></img> Infinity リリースイベント情報</h2>
+        </header>
+    );
+  }
+  
+  export default Heaader;
+  
