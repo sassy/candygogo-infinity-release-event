e1b76cafd730b811d4b23dd870d85355435b3e0f
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-07-24 02:24:51 +0900

    fix warning for img tag.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import Logo from '../images/header_logo.jpg'
 function Heaader() {
     return (
         <header className="App-header">
-            <h2><img className="Header-logo" src={Logo}></img> Infinity リリースイベント情報</h2>
+            <h2><img className="Header-logo" alt="logo" src={Logo}></img> Infinity リリースイベント情報</h2>
         </header>
     );
   }
